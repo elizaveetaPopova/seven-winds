5811ea041b43879d305013f17c4d329c4430f1f9
elibychkova <eli.bychkova@gmail.com>
2023-12-26 16:52:29 +0300

    added mockData

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,31 @@ import { ThemeProvider, createTheme } from '@mui/material';
 import Header from './components/Header';
 import NavigationPanel from './components/NavigationPanel';
 import { customPalette } from './palette';
+import { cssVar } from './functions';
+import Dashboard from './components/Dashboard';
 
 const Wrapper = styled.div`
   background-color: var(--main-bg-color);
+  height: 100vh;
 `;
 
 function App() {
-  const [theme] = useState(() => createTheme(customPalette));
+  const [theme] = useState(() =>
+    createTheme({
+      palette: {
+        primary: {
+          main: cssVar('--text-color-main'),
+        },
+      },
+    }),
+  );
+
   return (
     <ThemeProvider theme={theme}>
       <Wrapper>
         <Header />
         <NavigationPanel />
+        <Dashboard />
       </Wrapper>
     </ThemeProvider>
   );
---
+++ src/mockData.ts
@@ -0,0 +1,17 @@
+export const projects = [ 
+  { id: 1, title: 'По проекту', checked: false },
+  { id: 2, title: 'Объекты', checked: false },
+  { id: 3, title: 'РД', checked: false },
+  { id: 4, title: 'МТО', checked: false },
+  { id: 5, title: 'СМР', checked: true },
+  { id: 6, title: 'График', checked: false },
+  { id: 7, title: 'МиМ', checked: false },
+  { id: 8, title: 'Рабочие', checked: false },
+  { id: 9, title: 'Капвложения', checked: false },
+  { id: 10, title: 'Бюджет', checked: false },
+  { id: 11, title: 'Финансирование', checked: false },
+  { id: 12, title: 'Панорамы', checked: false },
+  { id: 13, title: 'Камеры', checked: false },
+  { id: 14, title: 'Поручения', checked: false },
+  { id: 15, title: 'Контрагенты', checked: false },
+]; 
